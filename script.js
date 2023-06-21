7a5e9610233879fa32d1de74238310e13916b57e
Vitor2008 <gabrielronaldo9343@gmail.com>
2023-06-20 21:16:15 -0400

    add index.txt

---
+++ script.js
@@ -0,0 +1,15 @@
+function iniciarConversa() {
+    var phone = document.getElementById('phone').value;
+    var ddd = document.getElementById('ddd').value;
+    
+    // Verificar se os campos estão preenchidos
+    if (phone && ddd) {
+      // Construir o link do WhatsApp
+      var whatsappLink = 'https://api.whatsapp.com/send?phone=' + ddd + phone;
+      
+      // Redirecionar o usuário para o WhatsApp
+      window.location.href = whatsappLink;
+    } else {
+      alert('Por favor, preencha todos os campos!');
+    }
+  }
